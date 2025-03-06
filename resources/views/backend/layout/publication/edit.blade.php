b7ab5f577b7f44be164663a7d1a9f6ba3b0db780
ALMAMON193 <mamon193p@gmail.com>
2025-03-06 10:10:35 +0600

    update summarnot in publication

--- resources/views/backend/layout/publication/edit.blade.php
+++ resources/views/backend/layout/publication/edit.blade.php
@@ -155,13 +155,19 @@
 @endsection
 
 @push('script')
+    <script src="https://cdn.ckeditor.com/ckeditor5/41.2.0/classic/ckeditor.js"></script>
     <script>
-        $('#description').summernote({
-            placeholder: 'Enter description...',
-            tabsize: 2,
-            height: 100
+        document.addEventListener('DOMContentLoaded', function() {
+            ClassicEditor
+                .create(document.querySelector('#description'), {
+                    height: '500px'
+                })
+                .catch(error => {
+                    console.error(error);
+                });
         });
-
+    </script>
+    <script>
         function previewDocument(event) {
             const file = event.target.files[0];
             const documentPreview = document.querySelector('#documentPreview');
